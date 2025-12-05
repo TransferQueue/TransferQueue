b09e0c89bf6430383ac7b85a2165707eb76e56d9
0oshowero0 <o0shower0o@outlook.com>
2025-12-05 11:26:13 +0800

    fix TODO
    Signed-off-by: 0oshowero0 <o0shower0o@outlook.com>

--- transfer_queue/utils/serial_utils.py
+++ transfer_queue/utils/serial_utils.py
@@ -134,7 +134,6 @@ class MsgpackDecoder:
             assert 0 in shape
             return torch.empty(shape, dtype=torch_dtype)
         # Create uint8 array and convert read-only buffer into writable bytearray
-        # TODO: Test whether this is safe.
         arr = torch.frombuffer(bytearray(buffer), dtype=torch.uint8)
         # Convert back to proper shape & type
         return arr.view(torch_dtype).view(shape)
