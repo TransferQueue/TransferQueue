c16cb4ec759f65af8c11d5fc79fabc79572cc1ec
0oshowero0 <o0shower0o@outlook.com>
2025-12-11 22:08:46 +0800

    add select_fields function for BatchMeta&FieldMeta
    Signed-off-by: 0oshowero0 <o0shower0o@outlook.com>

--- transfer_queue/client.py
+++ transfer_queue/client.py
@@ -365,7 +365,8 @@ class AsyncTransferQueueClient:
                 "Call initialize_storage_manager() before performing storage operations."
             )
 
-        if not metadata or metadata.size == 0:
+        if not metadata or metadata.size == 0 or len(metadata._field_names) == 0:
+            logger.warning(f"[{self.client_id}]: Empty BatchMeta provided to get_data. Returning empty TensorDict.")
             return TensorDict({}, batch_size=0)
 
         results = await self.storage_manager.get_data(metadata)
