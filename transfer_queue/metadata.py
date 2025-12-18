73dfb43ea2d2aab970949fb825c483be28ab9d69
0oshowero0 <o0shower0o@outlook.com>
2025-12-18 16:24:22 +0800

    fix
    Signed-off-by: 0oshowero0 <o0shower0o@outlook.com>

--- transfer_queue/metadata.py
+++ transfer_queue/metadata.py
@@ -271,9 +271,9 @@ class BatchMeta:
         for idx, sample in enumerate(self.samples):
             sample.add_fields(fields=fields[idx])
 
-            # Update batch-level fields cache
-            object.__setattr__(self, "_field_names", sorted(self.samples[0].field_names))
-            object.__setattr__(self, "_is_ready", all(sample.is_ready for sample in self.samples))
+        # Update batch-level fields cache
+        object.__setattr__(self, "_field_names", sorted(self.samples[0].field_names))
+        object.__setattr__(self, "_is_ready", all(sample.is_ready for sample in self.samples))
         return self
 
     def select_samples(self, sample_indices: list[int]) -> "BatchMeta":
