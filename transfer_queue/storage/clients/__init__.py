9bc1965c94be300519a43cef167257e2cdb1083f
0oshowero0 <o0shower0o@outlook.com>
2025-12-30 19:13:32 +0800

    update copyright
    Signed-off-by: 0oshowero0 <o0shower0o@outlook.com>

--- transfer_queue/storage/clients/__init__.py
+++ transfer_queue/storage/clients/__init__.py
@@ -1,3 +1,4 @@
+# Copyright 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
 # Copyright 2025 The TransferQueue Team
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- transfer_queue/storage/managers/factory.py
+++ transfer_queue/storage/managers/factory.py
@@ -1,3 +1,4 @@
+# Copyright 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
 # Copyright 2025 The TransferQueue Team
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from typing import Any
 
 from transfer_queue.storage.managers.base import TransferQueueStorageManager
